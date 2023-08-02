184bb3a3103e564689da81266e0bd0c0e89809cf
Robert Lynch <info@cayworks.com>
2023-08-01 20:43:12 -0500

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "cihmexplorer-41894",
-  url: "https://cihmexplorer-41894.botics.co",
-  api: "https://cihmexplorer-41894.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "cihmexplorer-41894",
+  "url": "https://cihmexplorer-41894.botics.co",
+  "api": "https://cihmexplorer-41894.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "Articles"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,22 +1,55 @@
-import React from "react"
-import { View, Image, Text, ScrollView, SafeAreaView } from "react-native"
-import { styles } from "./styles"
+import React from "react";
+import { View, Image, Text, ScrollView, SafeAreaView } from "react-native";
 
 const WelcomeScreen = () => {
-  return (
-    <SafeAreaView style={styles.container}>
+  return <SafeAreaView style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollView}>
         <View style={styles.group} />
         <View style={styles.group}>
-          <Image style={styles.logo} source={require("./logo.png")} />
-          <Text style={styles.text}>
-            Let's build something amazing together!
-          </Text>
+          <Image style={styles.logo} source={require("./CAYMAN ISLANDS REAL ESTATE MARKET EXPLORER LOGO BLACK.png")} />
+          <Text style={styles.text}>{"Welcome to the \nCayman Islands \nReal Estate Market Explorer!\n          "}</Text>
+          <Text style={styles.instructions}>{"\n            Explore properties, find your dream home, and make informed decisions\n            about the Real Estate Market in the Cayman Islands.\n          "}</Text>
         </View>
         <Text style={styles.footer}>Made with ❤️ by Crowdbotics</Text>
       </ScrollView>
-    </SafeAreaView>
-  )
-}
+    </SafeAreaView>;
+};
 
-export default WelcomeScreen
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: "#fff"
+  },
+  scrollView: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20
+  },
+  group: {
+    alignItems: "center",
+    marginBottom: 20
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 10
+  },
+  instructions: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 20
+  },
+  footer: {
+    fontSize: 12,
+    color: "#888",
+    textAlign: "center"
+  }
+};
+export default WelcomeScreen;
